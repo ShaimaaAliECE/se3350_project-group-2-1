820eb4532154a1b7c04b738b8644968cfd2ce591
samweller24 <samweller24@gmail.com>
2022-01-25 18:17:02 -0500

    Num Gen function

---
+++ sorting_game/src/utils/numberGenerator.js
@@ -0,0 +1,15 @@
+//function
+function createArray(size, range) {
+    //declare array
+    var a=[];
+    //for loop to iterate over array
+    for (i=0;i<size;++i) {
+        //create random value and set in array
+        let value = Math.floor(Math.random() * (range + 1));
+        a[i] = value;
+    }
+    //return array
+    return a;
+}
+//export function
+module.export = createArray;
