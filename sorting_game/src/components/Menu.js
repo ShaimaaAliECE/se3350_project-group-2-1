51174245f0189fb9b6abb736684674f967451eb3
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2022-01-28 11:37:37 -0500

    added comments and fixed

--- sorting_game/src/components/Menu.js
+++ sorting_game/src/components/Menu.js
@@ -12,54 +12,71 @@ const EmptyBox = (props) => {
 
 const Box = (props) => {
     const [isClicked, setClicked] = useState(false)
-    
+    let state = store.getState()
     const click = () => {
+        //flip the element as being either clicked or not clicked with true/false
         setClicked((isClicked) ? (false) : (true))
         
-        if(props.clickedArray.includes(props.element)){
-            props.setClickedArray(props.clickedArray.filter(element => props.element !== element))
+        let newArr = new Array()
+
+        //if the state array already contains the element, then remove it, id'd with its key
+        if (state.clickedArray.includes(props.element)) {
+            
+            newArr = state.clickedArray.filter(element => props.id !== element.id)
         }
-        else{
-            props.setClickedArray([...props.clickedArray, props.element])
+        //if the state array doesnt contain the element, add it to the clicked array
+        else {
+            newArr = [...state.clickedArray, {element: props.element, id: props.id}]
         }
+        
+        //store the new array into the store
+        store.dispatch({
+            type: "ChangeClickedArray",
+            payload: {
+                array: newArr
+            }
+        })
     }
 
+    useEffect(()=> {
+        console.log(state.clickedArray)
+    })
+
     return (
-        <div className={'cell'} style = {(isClicked) ? ({backgroundColor: 'red'}) : ({backgroundColor: 'blue'})} onClick={() => click()}>
+        <div 
+            className={'cell'} 
+            style={(isClicked) ? ({ backgroundColor: 'red' }) : ({ backgroundColor: 'blue' })} 
+            onClick={() => click()}
+        >
             {props.element}
         </div>
     )
 }
 
 const GridContainer = () => {
+    //state for which side component is rendering, set to left, right or middle, and the render comps will change
     const [side, setSide] = useState('middle')
-
-    const [clickedArray, setClickedArray] = useState(new Array())
 
     let state = store.getState()
 
-    let arrJSX = (state.obj[new String(state.currentStep)].array)
-
-    useEffect(()=> {
-        console.log(clickedArray)
-    })
+    //current array rendering 
+    let arrJSX = (state.stepsObject[new String(state.currentStep)].array)
 
     return (
         <div>
             {(side === 'middle') ? (
+                /* middle rendering*/
                 <div className='grid'>
                     {arrJSX.map((element, i) => {
-                        return (<Box key={i} element={element} allign='row' setClickedArray={setClickedArray} clickedArray={clickedArray} />)
+                        return (<Box key={i} id={i} element={element} allign='row'/>)
                     })}
-                    {
-
-                    }
-
                 </div>
             ) : (
                 (side === 'left') ? (
+                    /* left side rendering*/
                     <></>
                 ) : (
+                    /* right side side rendering*/
                     <></>
                 )
             )}
@@ -79,13 +96,16 @@ export default class Game extends Component {
     }
 
     startGame = () => {
+        /*  
+            on button click, load the new array into the Reducer from the create array 
+        */
         store.dispatch({
             type: "LoadNew",
             payload: {
-                array: createArray(this.state.size, this.state.range)
+                array: createArray(new Number(this.state.size), new Number(this.state.range))
             }
         })
-
+        //render the game comp
         this.setState({
             gameIsActive: true
         })
