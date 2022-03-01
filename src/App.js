a009203bcc7d993b8901ab3ee1d771b2397e0c41
Andreas Mavroudis <48466716+amavroud@users.noreply.github.com>
2022-03-01 00:10:39 -0500

    set up the routing frame for switching between home page and game
    not connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Game from "./components/Game.jsx";
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 // App function
 function App() {
   return (
-    <Game />
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Game/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;
