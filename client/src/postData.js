5ebef73033c2b5ae819291a26bf50b4893c9fed3
sammcdougalll <62080027+sammcdougalll@users.noreply.github.com>
2022-03-29 11:13:24 -0400

    postData

--- client/src/postData.js
+++ client/src/postData.js
@@ -1,42 +1,88 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+//const port = process.argv[2] || 9000;
 
-function postData(url) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(url)
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((err) => {
-        setError(err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [url]);
 
-  const refetch = () => {
-    setLoading(true);
-    axios
-      .get(url)
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((err) => {
-        setError(err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+const server = http.createServer(function(request, response) {
+    //console.dir(request.param)
 
-  return { data, loading, error, refetch };
-}
+    if  (request.method == 'POST') {
+        //console.log('POST')
+        var body = ''
+        request.on('data', function(data) {
+            body += data
+        })
+        request.on('end', function() {
+            console.log('Body: ' + body);
+            body += '\n';
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.writeHead(200, {'Access-Control-Allow-Origin': '*'});            
+            response.end('post received with data: ' + body);
+            
+            if  (fs.existsSync('GameStats.txt')) {
+                fs.appendFileSync('GameStats.txt', body);
+                console.log("The GameStats.txt file was apended to!");
+            }
+            else {
+                fs.writeFile("GameStats.txt", body, function(err) {
+                    if  (err) {
+                        return console.log(err);
+                    }
+                    console.log("The GameStats.txt file was created!");
+                }); 
+            }
+        })
+    } 
+    else {
+        console.log('GET')
+        var html = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+            <title>test.html</title>
+            <script>
+                function testSendingStats() {
+                    var JSONCall = "";
+                    var JSONString = "";
+                    var xmlhttp = new XMLHttpRequest();
+                    JSONCall += "http://127.0.0.1:3000";
+                    JSONString += document.getElementById("GameStats_Text").value;
+                    xmlhttp.open("POST", JSONCall, false);
+                    xmlhttp.setRequestHeader("Content-Type", "text/html");
+                    try {
+                        xmlhttp.send(JSONString);	            
+                    }
+                    catch (err) {
+                        console.log("<br>Could not connect to the service.");
+                        return("");
+                    }
+                    if  (xmlhttp.status != 200 || xmlhttp.responseText == "") {
+                        console.log("No Response Provided from Server" + xmlhttp.statusText);
+                        return("");
+                    }               
+                    return(xmlhttp.responseText);
+                }
+            </script>
+        </head>
+        <body>
+        <div style="float:left;background-color:white;">
+            <input id="GameStats_Text" type="text" style="float:left;width:600px;height:50px;font-size:30px;margin-left:50px;margin-top:150px;"><br><br>
+            <input type="button" onclick="testSendingStats('These are the game stats');" style="float:left;margin-top:50px;height:100px;margin-left:200px;width:300px;background-color:navy;color:white" value="RUN TEST">
+        </div>
+        </body>
+        </html>`
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.end(html)
+    }
+});
 
-export default postData;
+const port = 3000
+const host = '127.0.0.1'
+server.listen(port, host)
+console.log(`Listening at http://${host}:${port}`)
